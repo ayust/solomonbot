0b661e71d3d7114c0182da76763d1156e1db6752
Amber Yust <amber.yust@gmail.com>
2020-12-06 21:22:54 -0800

    Add ability to specify bot key via environment variable

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import argparse
 import importlib
+import os
 
 sb = importlib.import_module("solomonbot")
 
 parser = argparse.ArgumentParser(
     description="A Discord bot for organizing competitive games."
 )
-parser.add_argument("--key", required=True, help="Discord bot authentication key")
+parser.add_argument("--key", help="Discord bot authentication key")
 args = parser.parse_args()
 
-sb.bot.run(args.key)
+sb.bot.run(args.key or os.getenv("DISCORD_BOT_KEY"))
